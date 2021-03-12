f03d446849ecdafbe3166185682157d6b63b34e4
Ethan <nget@oregonstate.edu>
2021-03-11 20:31:14 -0800

    Changed fibonacci_test to run from 1-20 to actually return the tests

--- fibonacci_test.py
+++ fibonacci_test.py
@@ -3,7 +3,7 @@ import unittest
 import fibonacci
 class fibonacci1(unittest.TestCase):
     def test_fibonacci(self):
-        for Firstname in range(1, 101):
+        for Firstname in range(1, 21):
             self.assertIsInstance(Firstname, int, 'Entry is not of the correct type!')
             self.assertTrue(Firstname, 'Entry is blank!')
             result = fibonacci.fibonacci(Firstname)
